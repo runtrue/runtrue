ccdba0543dba3948d942379542246d1a95619ecf
doout <baheer09@gmail.com>
2026-08-07 14:47:52 +0000

    Update 0.1.6 capsule golden

--- crates/compiler/src/tests/compile.rs
+++ crates/compiler/src/tests/compile.rs
@@ -311,7 +311,7 @@ fn shipped_example_has_a_generation_local_golden_capsule_digest() {
         .unwrap();
     assert_eq!(
         compilation.capsule_digest.to_string(),
-        "sha256:2e03d44c17c09453ca559dab4c79276bd2db85ff44fa287621f58434e6f1db15"
+        "sha256:cdb16da20270d0fcd9e76d9efd9400d1e7aae47ce61b204c729311c06de87daf"
     );
 }
 
